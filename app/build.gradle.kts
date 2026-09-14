ec3cb5699ea76e7f6cef149579cc7fc68b8bc46c
Pruthvi Raj <pruthviraj9721@gmail.com>
2026-09-14 21:58:58 +0530

    Setup frontend for backend integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    // Networking
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.1.0")
 }
